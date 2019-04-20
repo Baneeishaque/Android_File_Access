8c3ef464e3943f5e6e2e01d7e22425d0320a039b
Banee Ishaque K <baneeishaque@gmail.com>
2019-04-20 13:24:47 +0530

    Runtime Permissions Rev.2

--- app/src/main/java/learn2crack/androidfile/MainActivity.java
+++ app/src/main/java/learn2crack/androidfile/MainActivity.java
@@ -1,6 +1,6 @@
 package learn2crack.androidfile;
 
-import android.Manifest;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
@@ -14,7 +14,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.app.Activity;
 
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
@@ -44,33 +43,33 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View arg0) {
 
-                if (ContextCompat.checkSelfPermission(MainActivity.this,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                        != PackageManager.PERMISSION_GRANTED) {
-
-                    // Permission is not granted
-                    // Should we show an explanation?
-                    if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
-                            Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                        // Show an explanation to the user *asynchronously* -- don't block
-                        // this thread waiting for the user's response! After the user
-                        // sees the explanation, try again to request the permission.
-                    } else {
-                        // No explanation needed; request the permission
-                        ActivityCompat.requestPermissions(MainActivity.this,
-                                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                                PERMISSIONS_WRITE_EXTERNAL_STORAGE_CODE);
-                    }
-                } else {
-                    // Permission has already been granted
-                    writeFile();
-                }
-
-//                if (ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-//                    permissionWriteOK();
+//                if (ContextCompat.checkSelfPermission(MainActivity.this,
+//                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+//                        != PackageManager.PERMISSION_GRANTED) {
+//
+//                    // Permission is not granted
+//                    // Should we show an explanation?
+//                    if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+//                            Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+//                        // Show an explanation to the user *asynchronously* -- don't block
+//                        // this thread waiting for the user's response! After the user
+//                        // sees the explanation, try again to request the permission.
+//                    } else {
+//                        // No explanation needed; request the permission
+//                        ActivityCompat.requestPermissions(MainActivity.this,
+//                                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+//                                PERMISSIONS_WRITE_EXTERNAL_STORAGE_CODE);
+//                    }
 //                } else {
-//                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{WRITE_EXTERNAL_STORAGE}, PERMISSIONS_WRITE_EXTERNAL_STORAGE_CODE);
+//                    // Permission has already been granted
+//                    writeFile();
 //                }
+
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    permissionWriteOK();
+                } else {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{WRITE_EXTERNAL_STORAGE}, PERMISSIONS_WRITE_EXTERNAL_STORAGE_CODE);
+                }
             }
         });
         read.setOnClickListener(new View.OnClickListener() {
@@ -78,33 +77,33 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View arg0) {
 
-                if (ContextCompat.checkSelfPermission(MainActivity.this,
-                        Manifest.permission.READ_EXTERNAL_STORAGE)
-                        != PackageManager.PERMISSION_GRANTED) {
-
-                    // Permission is not granted
-                    // Should we show an explanation?
-                    if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
-                            Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                        // Show an explanation to the user *asynchronously* -- don't block
-                        // this thread waiting for the user's response! After the user
-                        // sees the explanation, try again to request the permission.
-                    } else {
-                        // No explanation needed; request the permission
-                        ActivityCompat.requestPermissions(MainActivity.this,
-                                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                                PERMISSIONS_READ_EXTERNAL_STORAGE_CODE);
-                    }
-                } else {
-                    // Permission has already been granted
-                    readFile();
-                }
-
-//                if (ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-//                    permissionReadOK();
+//                if (ContextCompat.checkSelfPermission(MainActivity.this,
+//                        Manifest.permission.READ_EXTERNAL_STORAGE)
+//                        != PackageManager.PERMISSION_GRANTED) {
+//
+//                    // Permission is not granted
+//                    // Should we show an explanation?
+//                    if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+//                            Manifest.permission.READ_EXTERNAL_STORAGE)) {
+//                        // Show an explanation to the user *asynchronously* -- don't block
+//                        // this thread waiting for the user's response! After the user
+//                        // sees the explanation, try again to request the permission.
+//                    } else {
+//                        // No explanation needed; request the permission
+//                        ActivityCompat.requestPermissions(MainActivity.this,
+//                                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+//                                PERMISSIONS_READ_EXTERNAL_STORAGE_CODE);
+//                    }
 //                } else {
-//                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{READ_EXTERNAL_STORAGE}, PERMISSIONS_READ_EXTERNAL_STORAGE_CODE);
+//                    // Permission has already been granted
+//                    readFile();
 //                }
+
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    permissionReadOK();
+                } else {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{READ_EXTERNAL_STORAGE}, PERMISSIONS_READ_EXTERNAL_STORAGE_CODE);
+                }
             }
         });
     }
@@ -140,84 +139,84 @@ public class MainActivity extends Activity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSIONS_READ_EXTERNAL_STORAGE_CODE: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-                    readFile();
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                    Log.d(DEBUG_TAG, "Error : No Read permission");
-                }
-                break;
-            }
-            case PERMISSIONS_WRITE_EXTERNAL_STORAGE_CODE: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-                    writeFile();
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                    Log.d(DEBUG_TAG, "Error : No Write permission");
-                }
-                break;
-            }
-            // other 'case' lines to check for other
-            // permissions this app might request.
-        }
-
 //        switch (requestCode) {
-//            case PERMISSIONS_READ_EXTERNAL_STORAGE_CODE:
-//                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                    permissionReadOK();
+//            case PERMISSIONS_READ_EXTERNAL_STORAGE_CODE: {
+//                // If request is cancelled, the result arrays are empty.
+//                if (grantResults.length > 0
+//                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    // permission was granted, yay! Do the
+//                    // contacts-related task you need to do.
+//                    readFile();
 //                } else {
-//
-//                    Log.d(DEBUG_TAG, "Permission Not OK");
-//
-//                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//                        if (shouldShowRequestPermissionRationale(READ_EXTERNAL_STORAGE)) {
-//                            showMessageOKCancel("You need to allow read access to external storage",
-//                                    new DialogInterface.OnClickListener() {
-//                                        @Override
-//                                        public void onClick(DialogInterface dialog, int which) {
-//                                            requestPermissions(new String[]{READ_EXTERNAL_STORAGE},
-//                                                    PERMISSIONS_READ_EXTERNAL_STORAGE_CODE);
-//                                        }
-//                                    });
-//                        }
-//                    }
+//                    // permission denied, boo! Disable the
+//                    // functionality that depends on this permission.
+//                    Log.d(DEBUG_TAG, "Error : No Read permission");
 //                }
 //                break;
-//
-//            case PERMISSIONS_WRITE_EXTERNAL_STORAGE_CODE:
-//                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                    permissionWriteOK();
+//            }
+//            case PERMISSIONS_WRITE_EXTERNAL_STORAGE_CODE: {
+//                // If request is cancelled, the result arrays are empty.
+//                if (grantResults.length > 0
+//                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    // permission was granted, yay! Do the
+//                    // contacts-related task you need to do.
+//                    writeFile();
 //                } else {
-//
-//                    Log.d(DEBUG_TAG, "Permission Not OK");
-//
-//                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//                        if (shouldShowRequestPermissionRationale(WRITE_EXTERNAL_STORAGE)) {
-//                            showMessageOKCancel("You need to allow write access to external storage",
-//                                    new DialogInterface.OnClickListener() {
-//                                        @Override
-//                                        public void onClick(DialogInterface dialog, int which) {
-//                                            requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE},
-//                                                    PERMISSIONS_WRITE_EXTERNAL_STORAGE_CODE);
-//                                        }
-//                                    });
-//                        }
-//                    }
+//                    // permission denied, boo! Disable the
+//                    // functionality that depends on this permission.
+//                    Log.d(DEBUG_TAG, "Error : No Write permission");
 //                }
 //                break;
+//            }
+//            // other 'case' lines to check for other
+//            // permissions this app might request.
 //        }
+
+        switch (requestCode) {
+            case PERMISSIONS_READ_EXTERNAL_STORAGE_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    permissionReadOK();
+                } else {
+
+                    Log.d(DEBUG_TAG, "Permission Not OK");
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (shouldShowRequestPermissionRationale(READ_EXTERNAL_STORAGE)) {
+                            showMessageOKCancel("You need to allow read access to external storage",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            requestPermissions(new String[]{READ_EXTERNAL_STORAGE},
+                                                    PERMISSIONS_READ_EXTERNAL_STORAGE_CODE);
+                                        }
+                                    });
+                        }
+                    }
+                }
+                break;
+
+            case PERMISSIONS_WRITE_EXTERNAL_STORAGE_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    permissionWriteOK();
+                } else {
+
+                    Log.d(DEBUG_TAG, "Permission Not OK");
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (shouldShowRequestPermissionRationale(WRITE_EXTERNAL_STORAGE)) {
+                            showMessageOKCancel("You need to allow write access to external storage",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE},
+                                                    PERMISSIONS_WRITE_EXTERNAL_STORAGE_CODE);
+                                        }
+                                    });
+                        }
+                    }
+                }
+                break;
+        }
     }
 
     private void permissionWriteOK() {
